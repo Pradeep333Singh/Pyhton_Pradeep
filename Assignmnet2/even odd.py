a204334ade61d80504e900592f0a8cfa6f1db243
pradeep333singh <pradeep333singh@gmail.com>
2025-07-24 17:53:26 +0530

    my first commit

---
+++ Assignmnet2/even odd.py
@@ -0,0 +1,7 @@
+#Program to check if a number is even or odd
+
+num1 = int(input("Enter an Integer: "))
+if num1 % 2 == 0:
+    print(num1, " is an even number")
+else:
+    print(num1, " is an odd number")
