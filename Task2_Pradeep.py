7bccdda61f073999f665f3ded165c2afab10cd92
Pradeep333Singh <pradeep333singh@gmail.com>
2025-07-24 17:41:23 +0530

    Delete Task2_Pradeep.py

--- Task2_Pradeep.py
+++
@@ -1,4 +0,0 @@
-#Create a Personalized Greeting
-First_name = input("Enter your First name: ", )
-Last_name = input("Enter your Last name: ", )
-print("Hello, %s %s! Welcome to the Python Program."%(First_name,Last_name))
